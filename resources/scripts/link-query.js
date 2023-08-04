4a9dca5ac8855d4f44169c609d597400e0c946c4
the Black Cat <contact@theblack.cat>
2023-08-04 10:39:32 +0200

    v0.8

--- resources/scripts/link-query.js
+++ resources/scripts/link-query.js
@@ -1,39 +1,3 @@
-function toggleTBCDetails() {
-    // ... Your existing function code ...
-
-    // Append a query parameter to the URL when showing tBC Char Links
-    if (window.history && window.history.pushState) {
-        const url = window.location.href;
-        const urlWithQueryParam = url + (url.includes("?") ? "&" : "?") + "link=tbc";
-        window.history.pushState({ path: urlWithQueryParam }, "", urlWithQueryParam);
-    }
-}
-
-// Function to toggle the visibility of the Author Links section
-function toggleAuthorDetails() {
-    // ... Your existing function code ...
-
-    // Append a query parameter to the URL when showing Author Links
-    if (window.history && window.history.pushState) {
-        const url = window.location.href;
-        const urlWithQueryParam = url + (url.includes("?") ? "&" : "?") + "link=author";
-        window.history.pushState({ path: urlWithQueryParam }, "", urlWithQueryParam);
-    }
-}
-
-// Function to toggle the visibility of the Galleries section
-function toggleGalleryDetails() {
-    // ... Your existing function code ...
-
-    // Append a query parameter to the URL when showing Galleries
-    if (window.history && window.history.pushState) {
-        const url = window.location.href;
-        const urlWithQueryParam = url + (url.includes("?") ? "&" : "?") + "link=galleries";
-        window.history.pushState({ path: urlWithQueryParam }, "", urlWithQueryParam);
-    }
-}
-
-// Function to handle showing divs based on the "link" query parameter
 function showDivsBasedOnQueryParam() {
     const urlParams = new URLSearchParams(window.location.search);
     const link = urlParams.get('link');
