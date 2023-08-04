08f82637696da52483665c36dbe8b0f6229a2e24
the Black Cat <contact@theblack.cat>
2023-08-05 00:47:47 +0200

    JS fix attempt

---
+++ resources/scripts/index-links.js
@@ -0,0 +1,37 @@
+// the Black Cat
+
+document.getElementById('random-commwork-image').onclick = function() {
+  window.location.href = 'commissioned-works';
+};
+
+document.getElementById('tou').onclick = function() {
+    window.location.href = 'tou';
+};
+
+document.getElementById('wcif').onclick = function() {
+    window.location.href = 'wcif';
+};
+
+// Author
+
+document.getElementById('contact').onclick = function() {
+    window.location.href = 'contact';
+};
+
+document.getElementById('ccat-profile').onclick = function() {
+    window.open('https://curiouscat.live/theBlackCat_OC', '_blank');
+  };
+  
+// Galleries
+
+document.getElementById('body-reference-gallery').onclick = function() {
+    window.location.href = 'https://reference.theblack.cat';
+};
+
+document.getElementById('skeb-reference-gallery').onclick = function() {
+    window.location.href = 'https://skeb.theblack.cat';
+};
+
+document.getElementById('gdrive-gallery-folder').onclick = function() {
+    window.open('https://drive.google.com/drive/folders/1K-FQ2PfoNG9M1YeGzKJPGrv5uKqa649W?usp=drive_link', '_blank');
+  };
