f67010eeed9d6fcd802a414d7e2c988ae62d228b
the Black Cat <contact@theblack.cat>
2023-08-06 20:18:00 +0200

    Added click-to-hide to bubble-gallery

--- resources/scripts/bubble-begone.js
+++ resources/scripts/bubble-begone.js
@@ -26,3 +26,32 @@ function hideBubble() {
   bubble.style.display = 'none';
   bubbleVisible = false;
 }
+
+const bubbleGalleryTrigger = document.querySelector('.bubble-trigger-gallery');
+const bubbleGallery = document.querySelector('.bubble-gallery');
+let bubbleGalleryVisible = false;
+
+bubbleGalleryTrigger.addEventListener('click', () => {
+  if (bubbleGalleryVisible) {
+    hideBubbleGallery();
+  } else {
+    showBubbleGallery();
+  }
+});
+
+
+document.addEventListener('click', (event) => {
+  if (bubbleGalleryVisible && !bubbleGallery.contains(event.target) && !bubbleGalleryTrigger.contains(event.target)) {
+    hideBubble();
+  }
+});
+
+function showBubbleGallery() {
+  bubbleGallery.style.display = 'block';
+  bubbleGalleryVisible = true;
+}
+
+function hideBubbleGallery() {
+  bubbleGallery.style.display = 'none';
+  bubbleGalleryVisible = false;
+}
