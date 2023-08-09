7ca78f447206ec67619000f914ee3a9c9eabd2d7
the Black Cat <contact@theblack.cat>
2023-08-09 10:57:19 +0200

    v3.90: changed Start Page navigation logic

--- resources/scripts/tbc-selector.js
+++ resources/scripts/tbc-selector.js
@@ -54,6 +54,7 @@ function toggleAuthorDetails() {
   authorShowBackDiv.addEventListener('click', toggleAuthorBack);
 }
 
+// Function to toggle gallery details and hide other sections
 function toggleGalleryDetails() {
   // Get the elements by their IDs
   const galleriesShowTitleDiv = document.getElementById('galleries-show-title');
@@ -66,12 +67,12 @@ function toggleGalleryDetails() {
   const tbcDiv = document.getElementById('tbc');
   const authorDivContainer = document.getElementById('author');
 
-  // Remove the "display: none;" style from the gallery details
+  // Show gallery details
   galleriesShowTitleDiv.style.display = '';
   galleriesShowContainerDiv.style.display = '';
   galleriesShowBackDiv.style.display = '';
 
-  // Add the "display: none;" style to tbc details, linktree, tbc, and author
+  // Hide other sections
   tbcShowTitleDiv.style.display = 'none';
   tbcShowContainerDiv.style.display = 'none';
   tbcShowBackDiv.style.display = 'none';
@@ -82,9 +83,13 @@ function toggleGalleryDetails() {
   // Remove the existing click event listener from tbcShowBackDiv
   tbcShowBackDiv.removeEventListener('click', toggleTBCBack);
 
-  // Attach click event to the back button
+  // Attach click event to the back button for gallery details
   galleriesShowBackDiv.addEventListener('click', toggleGalleryBack);
 }
+
+// Set up event listener for the "Galleries" show button
+const galleriesShowButton = document.getElementById('galleries-show-button');
+galleriesShowButton.addEventListener('click', toggleGalleryDetails);
 
 function toggleTBCBack() {
   const tbcShowTitleDiv = document.getElementById('tbc-show-title');
@@ -126,18 +131,36 @@ function toggleAuthorBack() {
 
 }
 
+// Function to toggle back from gallery details and show other sections
 function toggleGalleryBack() {
   const galleriesShowTitleDiv = document.getElementById('galleries-show-title');
   const galleriesShowContainerDiv = document.getElementById('galleries-show-container');
   const galleriesShowBackDiv = document.getElementById('galleries-show-back');
+  const tbcShowTitleDiv = document.getElementById('tbc-show-title');
+  const tbcShowContainerDiv = document.getElementById('tbc-show-container');
+  const tbcShowBackDiv = document.getElementById('tbc-show-back');
   const linktreeContainerDiv = document.getElementById('linktree-container');
   const tbcDiv = document.getElementById('tbc');
   const authorDivContainer = document.getElementById('author');
 
+  // Hide gallery details
   galleriesShowTitleDiv.style.display = 'none';
   galleriesShowContainerDiv.style.display = 'none';
   galleriesShowBackDiv.style.display = 'none';
-  linktreeContainerDiv.style.display = '';
+
+  // Show other sections
+  tbcShowTitleDiv.style.display = '';
+  tbcShowContainerDiv.style.display = '';
+  tbcShowBackDiv.style.display = '';
+  linktreeContainerDiv.style.display = 'none';
   tbcDiv.style.display = '';
-  authorDivContainer.style.display = '';
+  authorDivContainer.style.display = 'none';
+
+  // Attach click event to the back button for TBC details
+  tbcShowBackDiv.addEventListener('click', toggleTBCBack);
 }
+
+// Set up event listener for the "Galleries" back button
+const galleriesShowBackDiv = document.getElementById('galleries-show-back');
+galleriesShowBackDiv.addEventListener('click', toggleGalleryBack);
+
