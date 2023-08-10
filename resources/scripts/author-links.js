8e050db4d27a84f664183efd27d9d8e897e30458
the Black Cat <contact@theblack.cat>
2023-08-10 21:40:47 +0200

    v3.91a

---
+++ resources/scripts/author-links.js
@@ -0,0 +1,19 @@
+document.getElementById('author-button-1').onclick = function() {
+    window.location.href = '#1';
+};
+
+document.getElementById('author-button-2').onclick = function() {
+    window.location.href = '#2';
+};
+
+document.getElementById('author-button-3').onclick = function() {
+    window.location.href = '#3';
+};
+
+document.getElementById('author-button-4').onclick = function() {
+    window.location.href = '#4';
+};
+
+document.getElementById('author-button-5').onclick = function() {
+    window.location.href = '#5';
+};
