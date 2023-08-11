23e279296bcfa6bc02d1cde7d5a9b75e8166cefb
the Black Cat <contact@theblack.cat>
2023-08-11 11:37:17 +0200

    Fixed undefined id on js scripts

---
+++ resources/scripts/gallery-links.js
@@ -0,0 +1,13 @@
+// Galleries
+
+document.getElementById('body-reference-gallery').onclick = function() {
+    window.location.href = 'https://reference.theblack.cat';
+};
+
+document.getElementById('skeb-reference-gallery').onclick = function() {
+    window.location.href = 'https://skeb.theblack.cat';
+};
+
+document.getElementById('gdrive-gallery-folder').onclick = function() {
+    window.open('https://drive.google.com/drive/folders/1K-FQ2PfoNG9M1YeGzKJPGrv5uKqa649W?usp=drive_link', '_blank');
+};
--- resources/scripts/index-links.js
+++ resources/scripts/index-links.js
@@ -11,27 +11,3 @@ document.getElementById('tou').onclick = function() {
 document.getElementById('wcif').onclick = function() {
     window.location.href = 'wcif';
 };
-
-// Author
-
-document.getElementById('contact').onclick = function() {
-    window.location.href = 'contact';
-};
-
-document.getElementById('ccat-profile').onclick = function() {
-    window.open('https://curiouscat.live/theBlackCat_OC', '_blank');
-  };
-  
-// Galleries
-
-document.getElementById('body-reference-gallery').onclick = function() {
-    window.location.href = 'https://reference.theblack.cat';
-};
-
-document.getElementById('skeb-reference-gallery').onclick = function() {
-    window.location.href = 'https://skeb.theblack.cat';
-};
-
-document.getElementById('gdrive-gallery-folder').onclick = function() {
-    window.open('https://drive.google.com/drive/folders/1K-FQ2PfoNG9M1YeGzKJPGrv5uKqa649W?usp=drive_link', '_blank');
-  };
