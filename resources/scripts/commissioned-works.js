b56c5c91443a4a82693bfdfcb1834735bbc8b578
the Black Cat <contact@theblack.cat>
2023-08-15 12:27:27 +0200

    Updated commissioned-works with new skeb art!

---
+++ resources/scripts/commissioned-works.js
@@ -0,0 +1,139 @@
+const tagContainer = document.getElementById('tagContainer');
+const gallery = document.getElementById('gallery');
+
+const tags = [
+  { name: 'NSFW', backgroundColor: '#ad1a00' },
+  { name: 'Suggestive', backgroundColor: '#f5a92f' },
+  { name: 'Erotica', backgroundColor: '#f75a2a' },
+  { name: 'Pornographic', backgroundColor: '#f72a2a' },
+  { name: 'Degen', backgroundColor: '#b62af7' },
+  { name: 'SafeForWork', backgroundColor: '#2ac955' },
+  { name: 'Fullbody', backgroundColor: '#f57dad' },
+  { name: 'Halfbody', backgroundColor: '#2af791' },
+  { name: 'Portrait', backgroundColor: '#2a83f7' },
+];
+
+function addTagToContainer(tag) {
+  const tagElement = document.createElement('div');
+  tagElement.classList.add('tag', 'tag-' + tag.name.toLowerCase());
+  tagElement.innerText = tag.name;
+  tagElement.style.backgroundColor = tag.backgroundColor;
+
+  tagElement.addEventListener('click', function () {
+    const isExclusiveTag = ['NSFW', 'Suggestive', 'Erotica', 'Pornographic', 'Degen', 'SafeForWork', 'Fullbody', 'Halfbody', 'Portrait'].includes(tag.name);
+    
+    // If the clicked tag is an exclusive tag, deselect all other tags in the same category.
+    if (isExclusiveTag) {
+      const exclusiveTags = ['NSFW', 'Suggestive', 'Erotica', 'Pornographic', 'Degen', 'SafeForWork', 'Fullbody', 'Halfbody', 'Portrait'];
+      for (const exclusiveTag of exclusiveTags) {
+        const exclusiveTagElement = document.querySelector('.tag-' + exclusiveTag.toLowerCase());
+        if (exclusiveTag !== tag.name) {
+          exclusiveTagElement.classList.remove('tag-selected');
+        }
+      }
+    }
+
+    // Toggle the clicked tag's selection status.
+    tagElement.classList.toggle('tag-selected');
+    
+    // Update the gallery based on the selected tags.
+    updateGallery();
+  });
+
+  tagContainer.appendChild(tagElement);
+}
+
+
+function updateGallery() {
+  const selectedTags = Array.from(tagContainer.getElementsByClassName('tag-selected')).map(tag => tag.innerText.toLowerCase());
+  const imageCards = document.querySelectorAll('.image-card');
+
+  if (selectedTags.length === 0) {
+    // If no tags are selected, hide the gallery
+    gallery.style.display = 'none';
+  } else {
+    gallery.style.display = 'flex';
+    for (const card of imageCards) {
+      const tagsInCard = card.dataset.tags.toLowerCase();
+      const displayCard = selectedTags.every(tag => tagsInCard.includes(tag));
+      card.style.display = displayCard ? 'block' : 'none';
+    }
+  }
+}
+
+function createTagElement(tag, imageCard, selected = false) {
+  const tagElement = document.createElement('div');
+  tagElement.classList.add('tag', selected ? 'tag-selected' : '');
+  tagElement.innerText = tag;
+
+  imageCard.appendChild(tagElement);
+}
+
+function generateTagsHTML(tags, isDescriptionTag = false) {
+  return tags
+    .split(' ')
+    .map(tag => `<div class="tag ${isDescriptionTag ? 'description-tag' : 'tag-selected'}">${tag}</div>`)
+    .join('');
+}
+
+function addTagsToCard(imageCard, tags) {
+  const imageInfo = imageCard.querySelector('.image-info');
+  const tagsParagraph = document.createElement('p');
+  tagsParagraph.classList.add('image-tags');
+
+  tagsParagraph.innerHTML = generateTagsHTML(tags, true);
+
+  imageInfo.appendChild(tagsParagraph);
+}
+
+function addImageToGallery(mediaSrc, tags, descriptionHTML) {
+    const imageCard = document.createElement('div');
+    imageCard.classList.add('image-card');
+    imageCard.dataset.tags = tags;
+  
+    const mediaElement = mediaSrc.includes('.mp4')
+      ? createVideoElement(mediaSrc)
+      : createImageElement(mediaSrc);
+  
+    const imageInfo = document.createElement('div');
+    imageInfo.classList.add('image-info');
+  
+    const descriptionParagraph = document.createElement('p');
+    descriptionParagraph.innerHTML = descriptionHTML;
+  
+    imageInfo.appendChild(descriptionParagraph);
+    imageCard.appendChild(mediaElement);
+    imageCard.appendChild(imageInfo);
+    gallery.appendChild(imageCard);
+  
+    addTagsToCard(imageCard, tags);
+}
+
+function createImageElement(imageSrc) {
+  const imgElement = document.createElement('img');
+  imgElement.src = imageSrc;
+  imgElement.alt = 'Image Description';
+  return imgElement;
+}
+
+function createVideoElement(videoSrc) {
+  const videoElement = document.createElement('video');
+  videoElement.src = videoSrc;
+  videoElement.controls = true;
+  return videoElement;
+}
+
+tags.forEach(tag => addTagToContainer(tag));
+
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/nikuyaking1.png', 'SafeForWork Suggestive Fullbody', 'Skeb commission by <a href="https://skeb.jp/@nikuyaking1" target="_blank">ニクヤキ</a>.');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/korodraws/korodraws-aug2023-clothed.png', 'SafeForWork Suggestive Fullbody', 'Fullbody Character commission by <a href="https://korodraws.carrd.co/" target="_blank">Koro</a>.<br>Clothed version.');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/korodraws/korodraws-aug2023-naked.png', 'NSFW Erotica', 'Fullbody Character commission by <a href="https://korodraws.carrd.co/" target="_blank">Koro</a>.<br>Naked version.');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/korodraws/korodraws-aug2023-full.png', 'NSFW Erotica', 'Fullbody Character Study commission by <a href="https://korodraws.carrd.co/" target="_blank">Koro</a>.<br>Full version.');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/ych-jul2023.png', 'NSFW Pornographic', 'YCH version 2A commission from <a href="https://iaminsamity.carrd.co/" target="_blank">SAMANTHA</a>.');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/ozzays041/ozzays041-v1.png', 'NSFW Degen', 'Fullbody commission from <a href="https://www.patreon.com/Ozzay041" target="_blank">MiFa</a>.<br><span class="latex-italic">Feeling it.</span>');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/ozzays041/ozzays041-v2.png', 'NSFW Degen', 'Fullbody commission from <a href="https://www.patreon.com/Ozzay041" target="_blank">MiFa</a>.<br><span class="latex-italic-bold">Really</span><span class="latex-italic"> Feeling it.</span>');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/ozzays041/ozzays041-v3.png', 'NSFW Degen', 'Fullbody commission from <a href="https://www.patreon.com/Ozzay041" target="_blank">MiFa</a>.<br><span class="latex-italic-bold">Cummin.</span>');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/hamo19574270-fullbody.png', 'NSFW Degen', 'Skeb commission from <a href="https://skeb.jp/@hamo19574270" target="_blank">é±§</a>.');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/chaosringen-fullrender-bust.png', 'SafeForWork Suggestive Portrait', 'Full render portrait commission by <a href="https://linktr.ee/chaosringen" target="_blank">chaosringen</a>');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/juurikun-fullbody-sketch.png', 'SafeForWork Fullbody', 'Sketchy fullbody artwork commission by <a href="https://www.instagram.com/juurikun/" target="_blank">juurikun</a><br>ðŸ™');
+addImageToGallery('https://cdn.theblack.cat/gallery/commissioned-works/chaosringen-sketch.jpg', 'SafeForWork Suggestive Portrait', 'Sketch for a full render portrait commission by <a href="https://linktr.ee/chaosringen" target="_blank">chaosringen</a>');
