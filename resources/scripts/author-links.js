10a3380cb6934fbf4236cabdc85fbe5ced8aa1ef
the Black Cat <contact@theblack.cat>
2023-08-16 15:02:52 +0200

    v3.97a

--- resources/scripts/author-links.js
+++ resources/scripts/author-links.js
@@ -1,19 +1,19 @@
-document.getElementById('author-button-1').onclick = function() {
+document.getElementById('author-button-twitter').onclick = function() {
     window.open('https://twitter.com/di_theBlackCat', '_blank');
   };
 
-  document.getElementById('author-button-2').onclick = function() {
+  document.getElementById('author-button-git').onclick = function() {
     window.open('https://github.com/theBlackCat-OC', '_blank');
   };
 
-  document.getElementById('author-button-3').onclick = function() {
+  document.getElementById('author-button-ccat').onclick = function() {
     window.open('https://curiouscat.live/theBlackCat_OC', '_blank');
   };
 
-document.getElementById('author-button-4').onclick = function() {
+document.getElementById('author-button-contact').onclick = function() {
     window.location.href = 'contact';
 };
 
-document.getElementById('author-button-5').onclick = function() {
+document.getElementById('author-button-blog').onclick = function() {
     window.location.href = '#soon';
 };
