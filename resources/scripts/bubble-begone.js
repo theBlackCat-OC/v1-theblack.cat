ab201e9d3ad30cbd39d9e47fe82d4ab29352baae
the Black Cat <contact@theblack.cat>
2023-08-22 09:08:56 +0200

    v4.0

--- resources/scripts/bubble-begone.js
+++ resources/scripts/bubble-begone.js
@@ -26,33 +26,3 @@ function hideBubble() {
   bubble.style.display = 'none';
   bubbleVisible = false;
 }
-
-
-const bubbleTriggerGallery = document.querySelector('.bubble-trigger-gallery');
-const bubbleGallery = document.querySelector('.bubble-gallery');
-let bubbleGalleryVisible = false;
-
-bubbleTriggerGallery.addEventListener('click', () => {
-  if (bubbleGalleryVisible) {
-    hideGalleryBubble();
-  } else {
-    showGalleryBubble();
-  }
-});
-
-
-document.addEventListener('click', (event) => {
-  if (bubbleGalleryVisible && !bubbleGallery.contains(event.target) && !bubbleTriggerGallery.contains(event.target)) {
-    hideGalleryBubble();
-  }
-});
-
-function showGalleryBubble() {
-  bubbleGallery.style.display = 'block';
-  bubbleGalleryVisible = true;
-}
-
-function hideGalleryBubble() {
-  bubbleGallery.style.display = 'none';
-  bubbleGalleryVisible = false;
-}
