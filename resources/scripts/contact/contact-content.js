3fce2d9abaf70facbd882d55cd9b2ed2f301e0ca
the Black Cat <contact@theblack.cat>
2023-08-25 12:28:34 +0200

    v.4.4c

--- resources/scripts/contact/contact-content.js
+++ resources/scripts/contact/contact-content.js
@@ -1,48 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const defaultContent = document.getElementById('contact-content-default');
-  
-    const emailButton = document.getElementById('contact-email-button');
-    const emailContent = document.getElementById('contact-content-email');
-  
-    const twitterButton = document.getElementById('contact-twitter-button');
-    const twitterContent = document.getElementById('contact-content-twitter');
-  
-    const discordButton = document.getElementById('contact-discord-button');
-    const discordContent = document.getElementById('contact-content-discord');
-  
-    const misskeyButton = document.getElementById('contact-misskey-button');
-    const misskeyContent = document.getElementById('contact-content-misskey');
-  
-    emailButton.addEventListener('click', function () {
-      emailContent.style.display = '';
-      defaultContent.style.display = 'none';
-      twitterContent.style.display = 'none';
-      discordContent.style.display = 'none';
-      misskeyContent.style.display = 'none';
+  const buttons = [
+    { button: document.getElementById('contact-email-button'), content: document.getElementById('contact-content-email') },
+    { button: document.getElementById('contact-twitter-button'), content: document.getElementById('contact-content-twitter') },
+    { button: document.getElementById('contact-discord-button'), content: document.getElementById('contact-content-discord') },
+    { button: document.getElementById('contact-misskey-button'), content: document.getElementById('contact-content-misskey') }
+  ];
+
+  const defaultContent = document.getElementById('contact-content-default');
+  defaultContent.style.display = 'none';
+
+  function showContent(selectedContent) {
+    buttons.forEach(item => {
+      item.content.style.display = (item === selectedContent) ? '' : 'none';
+      
+      if (item === selectedContent && item.button) {
+        item.button.classList.add('hovered');
+      } else {
+        item.button.classList.remove('hovered');
+      }
     });
-  
-    twitterButton.addEventListener('click', function () {
-      emailContent.style.display = 'none';
-      defaultContent.style.display = 'none';
-      twitterContent.style.display = '';
-      discordContent.style.display = 'none';
-      misskeyContent.style.display = 'none';
-    });
-  
-    discordButton.addEventListener('click', function () {
-      emailContent.style.display = 'none';
-      defaultContent.style.display = 'none';
-      twitterContent.style.display = 'none';
-      discordContent.style.display = '';
-      misskeyContent.style.display = 'none';
-    });
-  
-    misskeyButton.addEventListener('click', function () {
-      emailContent.style.display = 'none';
-      defaultContent.style.display = 'none';
-      twitterContent.style.display = 'none';
-      discordContent.style.display = 'none';
-      misskeyContent.style.display = '';
+
+    defaultContent.style.display = 'none';
+  }
+
+  buttons.forEach(item => {
+    item.button.addEventListener('click', () => {
+      showContent(item);
     });
   });
-  
+
+  defaultContent.style.display = '';
+});
